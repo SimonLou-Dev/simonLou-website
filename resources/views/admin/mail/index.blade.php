a713cc529d27f1c738980d3d1775f4551a3652ae
Simon Lou <simonlou-dev>
2020-12-10 20:48:05 +0100

    Mail header & add icon

--- resources/views/admin/mail/index.blade.php
+++ resources/views/admin/mail/index.blade.php
@@ -7,7 +7,46 @@
 @section('mail')
     <div class="mail-box">
         @yield('addsomeele')
+        <div class="mail-header">
+            <div class="mail-selected-infos">
+                <button class="check any">
+                    <img src="{{asset('assets/img/icons/empty_checkbox.png')}}">
+                </button>
+                <button class="check little">
+                    <img src="{{asset('assets/img/icons/line_checked_chekbob.png')}}">
+                </button>
+                <button class="check all">
+                    <img src="{{asset('assets/img/icons/checked_chekbox.png')}}">
+                </button>
+                <button class="add_to_fav selected">
+                    <img src="{{asset('assets/img/icons/star.png')}}">
+                </button>
+                <button class="more_infos">
+                    <img src="{{asset('assets/img/icons/three_points.png')}}">
+                </button>
+            </div>
+            <div class="pagination">
+                    <a href=""><img src="{{asset('assets/img/icons/right_arrow_head.png')}}"> </a>
+                    <p>90-100/100</p>
+                    <a href=""><img src="{{asset('assets/img/icons/right_arrow_head.png')}}"></a>
+            </div>
+        </div>
         <div class="mail" data-location="{{route('admin.mail.read')}}">
+            <div class="action mail-content">
+                <h2><a href=""> <i class="far fa-star"></i></a></h2>
+                <h2><a href=""><i class="fas fa-reply"></i></a></h2>
+            </div>
+            <div class="sender mail-content">
+                <h2>Pas Nous</h2>
+            </div>
+            <div class="object mail-content">
+                <h2>C'est un text</h2>
+            </div>
+            <div class="date mail-content">
+                <h2>17/29/1000 à 00:00</h2>
+            </div>
+        </div>
+        <div class="mail new" data-location="{{route('admin.mail.read')}}">
             <div class="action mail-content">
                 <h2><a href=""> <i class="far fa-star"></i></a></h2>
                 <h2><a href=""><i class="fas fa-reply"></i></a></h2>
@@ -52,7 +91,7 @@
                 <h2>17/29/1000 à 00:00</h2>
             </div>
         </div>
-        <div class="mail" data-location="{{route('admin.mail.read')}}">
+        <div class="mail new" data-location="{{route('admin.mail.read')}}">
             <div class="action mail-content">
                 <h2><a href=""> <i class="far fa-star"></i></a></h2>
                 <h2><a href=""><i class="fas fa-reply"></i></a></h2>
@@ -82,22 +121,7 @@
                 <h2>17/29/1000 à 00:00</h2>
             </div>
         </div>
-        <div class="mail" data-location="{{route('admin.mail.read')}}">
-            <div class="action mail-content">
-                <h2><a href=""> <i class="far fa-star"></i></a></h2>
-                <h2><a href=""><i class="fas fa-reply"></i></a></h2>
-            </div>
-            <div class="sender mail-content">
-                <h2>Pas Nous</h2>
-            </div>
-            <div class="object mail-content">
-                <h2>C'est un text</h2>
-            </div>
-            <div class="date mail-content">
-                <h2>17/29/1000 à 00:00</h2>
-            </div>
-        </div>
-        <div class="mail" data-location="{{route('admin.mail.read')}}">
+        <div class="mail new" data-location="{{route('admin.mail.read')}}">
             <div class="action mail-content">
                 <h2><a href=""> <i class="far fa-star"></i></a></h2>
                 <h2><a href=""><i class="fas fa-reply"></i></a></h2>
@@ -233,6 +257,8 @@
             </div>
         </div>
     </div>
+
+
     <script type="text/javascript">
         $(document).ready(function (){
             $('.mail').click(function (event){
--- resources/views/admin/mail/layout/mail.blade.php
+++ resources/views/admin/mail/layout/mail.blade.php
@@ -7,10 +7,11 @@
             <div class="mail-head">
                 <nav>
                     <ul>
-                        <li><a href="{{route('admin.mail')}}">Accueil</a></li>
-                        <li><a href="{{route('admin.mail')}}">Envoyés</a></li>
-                        <li><a href="{{route('admin.mail.create')}}">Nouveau</a></li>
-                        <li><a href="" class="start"><i class="far fa-star"></i></a></li>
+                        <li><a href="{{route('admin.mail.create')}}">écrire</a></li>
+                        <li><a href="{{route('admin.mail')}}">brouillions</a></li>
+                        <li><a href="{{route('admin.mail')}}">envoyés</a></li>
+                        <li><a href="" class="start"><i class="far fa-star"></i>favoris</a></li>
+                        <li><a href="">libéllées</a></li> // Dropdown
                     </ul>
                 </nav>
                 <div class="text"><h2>Messagerie</h2></div>
